06893c255ec631551735443daa1f8190bcaa6e4c
Oscar <github@oscarb.se>
2016-04-12 13:15:29 +0200

    Add GSON for converting JSON to Java Objects

--- app/src/main/java/se/oscarb/movieapidemo/MainActivity.java
+++ app/src/main/java/se/oscarb/movieapidemo/MainActivity.java
@@ -11,7 +11,11 @@ public class MainActivity extends AppCompatActivity {
            compile 'com.squareup.retrofit2:retrofit:2.0.1'
         2. Lägg in att vi använder oss av internet i AndroidManifest.xml:
            <uses-permission android:name="android.permission.INTERNET" />
-        3.
+        3. För att konvertera informationen (från JSON till Java-objekt) så behöver vi GSON (en konverterare)
+           compile 'com.google.code.gson:gson:2.6.2'
+        3. Förbered för att hämta JSON-data - skapa en klass som motsvarar det vi hämtar
+           * Skapa klassen Movie
+           *
 
      */
 
---
+++ app/src/main/java/se/oscarb/movieapidemo/Movie.java
@@ -0,0 +1,12 @@
+package se.oscarb.movieapidemo;
+
+public class Movie {
+    // Instansvariabler
+    @Ser
+    private String title;
+    private int year;
+    private char rated;
+    private String released;
+
+
+}
