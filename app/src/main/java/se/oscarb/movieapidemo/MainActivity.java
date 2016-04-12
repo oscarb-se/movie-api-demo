1a13c0a5fdd66f0423672f6f5d884cee763d4b42
Oscar <github@oscarb.se>
2016-04-12 13:42:44 +0200

    Add Java interface for the API we want to communicate with

--- app/src/main/java/se/oscarb/movieapidemo/MainActivity.java
+++ app/src/main/java/se/oscarb/movieapidemo/MainActivity.java
@@ -1,7 +1,11 @@
 package se.oscarb.movieapidemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +17,16 @@ public class MainActivity extends AppCompatActivity {
            <uses-permission android:name="android.permission.INTERNET" />
         3. För att konvertera informationen (från JSON till Java-objekt) så behöver vi GSON (en konverterare)
            compile 'com.google.code.gson:gson:2.6.2'
-        3. Förbered för att hämta JSON-data - skapa en klass som motsvarar det vi hämtar
+        4. Förbered för att hämta JSON-data - skapa en klass som motsvarar det vi hämtar
            * Skapa klassen Movie
-           *
+           * Lägg till instansvariabler för det du vill komma åt i JSON-datan
+           * Skapa getters (get-metoder) för våra instansvariabler
+        5. Ta fram koden som gör så att Retrofit går in på en sida, hämtar data
+           och gör om det till ett objekt i Java!
+           Se metoden findMovie nedan
+        6. Skapa ett Interface i Java för det RESTful API vi vill kommunicera med
+
+
 
      */
 
@@ -23,5 +34,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Instansmetod
+    public void findMovie(View view) {
+        // Adressen vi utgår ifrån
+        String baseUrl = "http://www.omdbapi.com/";
+
+        // Skapa en instans av Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
     }
 }
--- app/src/main/java/se/oscarb/movieapidemo/Movie.java
+++ app/src/main/java/se/oscarb/movieapidemo/Movie.java
@@ -1,12 +1,37 @@
 package se.oscarb.movieapidemo;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Movie {
     // Instansvariabler
-    @Ser
-    private String title;
+    // I JSON står det "Title" men instansvariablen ska vara title
+
+    @SerializedName("Title")  // Från JSON
+    private String title;     // till Java
+
+    @SerializedName("Year")
     private int year;
+
+    @SerializedName("Rated")
     private char rated;
+
+    @SerializedName("Released")
     private String released;
 
+    // Instansmetoder
+    public String getTitle() {
+        return title;
+    }
 
+    public int getYear() {
+        return year;
+    }
+
+    public char getRated() {
+        return rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
 }
