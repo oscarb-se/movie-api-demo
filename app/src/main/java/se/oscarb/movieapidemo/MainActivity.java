866fbbaa5e1f95533c109d98111155dbb3596964
Oscar <github@oscarb.se>
2016-04-12 11:44:31 +0200

    Add Retrofit, permission to use internet and Views for getting a movie

--- app/src/main/java/se/oscarb/movieapidemo/MainActivity.java
+++ app/src/main/java/se/oscarb/movieapidemo/MainActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+        För att använda ett API
+        1. Lägg in Retrofit i build.gradle
+        2. Lägg in att vi använder oss av internet i manifestet:
+             <uses-permission android:name="android.permission.INTERNET" />
+
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
